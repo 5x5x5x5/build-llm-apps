2d88c3e9ffbf906b3b76afe9c177bb57eb596789
Daniel Siegle <daniel.siegle@gmail.com>
2023-07-13 13:43:41 -0400

    expanding main_page.py

--- main_page.py
+++ main_page.py
@@ -1,17 +1,34 @@
-# Contents of ~/my_app/main_page.py
 import streamlit as st
 
-st.markdown("# Main page 🎈")
-st.sidebar.markdown("# Main page 🎈")
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
 
-# Contents of ~/my_app/pages/page_2.py
-import streamlit as st
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
 
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
 
-# Contents of ~/my_app/pages/page_3.py
-import streamlit as st
+def page4():
+    st.markdown("# Page 4 🎉")
+    st.sidebar.markdown("# Page 4 🎉")
 
-st.markdown("# Page 3 🎉")
-st.sidebar.markdown("# Page 3 🎉")
+def page5():
+    st.markdown("# Page 5 🎉")
+    st.sidebar.markdown("# Page 5 🎉")
+
+def page6():
+    st.markdown("# Page 6 🎉")
+    st.sidebar.markdown("# Rage 6 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
